6e387473cf42579620b12d44a260cc63336dbffd
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-09-12 13:15:49 -0300

    finished dev configs

---
+++ server/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es2015: true,
+  },
+  extends: ['airbnb-base', 'prettier', 'prettier/@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['prettier', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'import/extensions': [
+      'warn',
+      {
+        '~/*': ['src/*'],
+      },
+    ],
+    'import/no-unresolved': 'off',
+  },
+};
